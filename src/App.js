a667c2153962cba4f41f76ea96f33cdd1af2d84e
jeffreycain <jeffjcain@gmail.com>
2024-05-07 09:16:30 -0500

    fix website for different screen sizes and add resume

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           <h3>Jeffrey Cain</h3>
           <h1>Software Developer</h1>
           <p>
-            Hello, I am Jeffrey Cain! I have been a Full Stack Software Developer since 2019 with a majority of my work experience focused on Web Development and modernizing legacy applications. I've used various frontend technologies including React, JQuery, Typescript, Next.js as well as basic javascript, while on the backend I have use primarily PHP and MySql for database management.
+            Hello, I am Jeffrey Cain! I have been a Full Stack Software Developer since 2019 with a majority of my work experience focused on Web Development and modernizing legacy applications. I've used various frontend technologies including React, JQuery, Typescript, Next.js as well as basic javascript, while on the backend I have used primarily PHP and MySql for database management.
           </p>
           <p>
             I played in the Kansas State University Marching Band for 4 years, between 2015 and 2019 and am a big KSU sports fan. I played in band for the Football, Mens' Basketball, Womens' Basketball and Womens' Volleyball programs during my time there. I still keep up with how these programs are doing, with some of my favorite experiences being at #14 KSU v #5 Alabama in the Sugar Bowl (2022) and #9 KSU v #16 UMBC in the round of 32 for the NCAA tournament (2018).
@@ -28,7 +28,7 @@ function App() {
           <p>jeffjcain@gmail.com</p>
           <p><a href="https://github.com/jeffreycain">Github</a></p>
           <p><a href="https://www.linkedin.com/in/jeffrey-cain/">LinkedIn</a></p>
-          <p>Resume (add link)</p>
+          <p><a href='/content/JeffreyCainResume.pdf' download>Resume</a></p>
       </div>
     </div>
   );
